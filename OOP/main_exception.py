9543a1ddfb0c5ef11cb6ca4bce78d90cb5e078f5
Artem <ibnajub@gmail.com>
2022-12-06 11:41:12 +0200

    задание

--- OOP/main_exception.py
+++ OOP/main_exception.py
@@ -1,3 +1,15 @@
+# 1. Додати до класу Employee методи save_email(self) та validate(self) та атрибут email
+# 2. Створити виняток EmailAlreadyExistsException
+# 3. Метод save_email має викликатись в кінці методу __init__ та записувати email в файл emails.txt
+# 4. Метод validate має перевіряти чи існує імейл в файлі. Якщо імейл вже існує, то викликати помилку
+# EmailAlreadyExistsException
+# 5. В головному файлі програми (app.py або main.py) весь код, який знаходиться в блоці
+# if __name__ == ‘__main__’ перенести в нову функцію main() і викликати цю функцію в
+# name == main блоці.
+# 6. Огорнути виклик функції main() в блок try/except.
+# У разі виникнення винятку EmailAlreadyExistsException записати в файл logs.txt повідомлення у
+# вигляді: %дата% %час% | %traceback%
+
 
 import Lesson_exception1 as lesson_exception
 import expection_class
