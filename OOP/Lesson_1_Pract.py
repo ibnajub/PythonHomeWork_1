f1d419ddf591ee5f09c62f574e4cf3ea0154953f
Artem <ibnajub@gmail.com>
2022-12-07 18:35:00 +0200

    домашка 1

--- OOP/Lesson_1_Pract.py
+++ OOP/Lesson_1_Pract.py
@@ -11,14 +11,18 @@
 
 
 class Ellem:
-    def __int__(self, t_melting, t_boiling):
+    def __init__(self, t_melting, t_boiling):
         self.t_melting = t_melting
         self.t_boiling = t_boiling
 
-    def convert_state(self, temp):
-        if temp < self.t_melting:
+    def convert_state(self, temp, t_melting = None, t_boiling= None):
+        # запихнуть в def не дает так написать -- t_melting =self.t_melting
+        # NameError: name 'self' is not defined
+        t_melting = t_melting if t_melting != None else self.t_melting
+        t_boiling = t_boiling if t_boiling != None else self.t_boiling
+        if temp < t_melting:
             return 'solid'
-        if temp > self.t_boiling:
+        if temp > t_boiling:
             return 'vapour'
         return 'liquid'
 
@@ -26,10 +30,12 @@ class Ellem:
         if scale == 'K':
             return temp - 273
         if scale == 'F':
-            return (temp - 32) * 5 \ 9
+            return (temp - 32) * 5 / 9
         return temp
 
 
 if __name__ == '__main__':
-    fe = Ellem()
-print(fe.)
+    fe = Ellem(0, 100)
+    print(fe.convert_state(-100))
+    print(fe.convert_temp(100, 'K'))
+    print(fe.convert_state(-100, -1000,100))
